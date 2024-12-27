f16ae43db3e71ba3d77fbfbb39c6f253cc66362f
ValentinoSanchez00 <valentinosanchezraverta@gmail.com>
2024-12-27 12:10:15 +0100

    prueba webhook

--- src/plantilla/Navegacion.js
+++ src/plantilla/Navegacion.js
@@ -6,7 +6,7 @@ export default function Navegacion() {
     <div classNameName='container'>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Sistema de Recursos Humano</a>
+    <a className="navbar-brand" href="/">Sistema de Recursos Humanos</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
